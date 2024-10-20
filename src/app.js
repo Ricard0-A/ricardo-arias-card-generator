35ead14696c32c8eb3902692b06a1496ef1ad95a
Ricard0-A <ricardoarias4@hotmail.com>
2024-10-20 10:43:38 +0000

    done

--- src/app.js
+++ src/app.js
@@ -5,7 +5,52 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector(".card").classList.add(generateRsuit());
+  document.querySelector(".card").innerHTML = generateRnumber();
 };
+
+function generateRnumber() {
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  let indxNumber = Math.floor(Math.random() * numbers.length);
+  return numbers[indxNumber];
+}
+
+// ------------------------------------------------------------------
+
+function generateRsuit() {
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let suit = ["diamond", "spade", "heart", "club"];
+
+  let suitNumber = Math.floor(Math.random() * suit.length);
+  return suit[suitNumber];
+}
